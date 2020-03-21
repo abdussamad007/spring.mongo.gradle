519b8be5487a8854187b4b00794574c9309e6c48
Abdus Mondal <abdus.mondal@gmail.com>
2020-03-21 22:15:14 +0530

    Initial Version

---
+++ src/main/java/com/abdus/spring/mongo/gradle/example/dao/CustomerRepository.java
@@ -0,0 +1,11 @@
+package com.abdus.spring.mongo.gradle.example.dao;
+
+import com.abdus.spring.mongo.gradle.example.model.Customer;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface CustomerRepository extends MongoRepository<Customer,String> {
+  public Customer findByFirstName(String firstName);
+  public List<Customer> findByLastName(String lastName);
+}
---
+++ src/test/java/com/abdus/spring/mongo/gradle/example/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdus.spring.mongo.gradle.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
